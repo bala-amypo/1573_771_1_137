f6e7e93ca3842ea5899a98a4c521c0e9ae849d89
bala-amypo <bala@amypo.in>
2025-12-18 14:50:23 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,15 +1,30 @@
-@Bean
-public OpenAPI customOpenAPI() {
-    SecurityScheme securityScheme = new SecurityScheme()
-        .name("Authorization")
-        .type(SecurityScheme.Type.HTTP)
-        .scheme("bearer")
-        .bearerFormat("JWT");
+package com.example.demo.config;
 
-    return new OpenAPI()
-        .info(new Info().title("SaaS User Role Permission Manager API")
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SaaS User Role Permission Manager API")
                         .version("1.0")
                         .description("JWT secured role-permission management"))
-        .addSecurityItem(new SecurityRequirement().addList("Authorization"))
-        .schemaRequirement("Authorization", securityScheme);
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .schemaRequirement("Authorization", securityScheme);
+    }
 }
