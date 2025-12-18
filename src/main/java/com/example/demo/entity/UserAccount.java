02e5e2cb866dd346a358ea34de88f74809c8ebae
bala-amypo <bala@amypo.in>
2025-12-18 13:13:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -67,4 +67,13 @@ this.updatedAt = Instant.now();
 
 
 // Getters and Setters
+public Long getId() { return id; }
+public String getEmail() { return email; }
+public void setEmail(String email) { this.email = email; }
+public String getPassword() { return password; }
+public String getFullName() { return fullName; }
+public void setFullName(String fullName) { this.fullName = fullName; }
+public boolean getActive() { return active; }
+public void setActive(boolean active) { this.active = active; }
+
 }
