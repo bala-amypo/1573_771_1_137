52f0aba35ecadaa462b59cbe82d1603cd135917e
bala-amypo <bala@amypo.in>
2025-12-18 14:30:59 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,10 +15,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        // JWT Security Scheme
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                // You need to change the port as per your server
+                // Set server URL with port 9263
                 .servers(List.of(
                         new Server().url("https://9263.408procr.amypo.ai")
-                ));
-        }
+                ))
+                .info(new Info()
+                        .title("SaaS User Role Permission Manager API")
+                        .version("1.0")
+                        .description("JWT secured role-permission management"))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .schemaRequirement("Authorization", securityScheme);
+    }
 }
