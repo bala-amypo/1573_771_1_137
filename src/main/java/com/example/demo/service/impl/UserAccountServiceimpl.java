62cb8c741c1212e9083eabcea797fb79dc56af68
bala-amypo <bala@amypo.in>
2025-12-18 05:19:39 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserAccountServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceimpl.java
@@ -1,9 +1,14 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.UserRole;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.repository.UserRoleRepository;
+import com.example.demo.service.UserRoleService;
 
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.UserRole;
-import com.example.demo.exception.*;
-import com.example.demo.repository.*;
 
 import java.util.List;
 
@@ -38,7 +43,8 @@ public class UserRoleServiceImpl implements UserRoleService {
     @Override
     public UserRole getMappingById(Long id) {
         return userRoleRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Mapping not found"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Mapping not found"));
     }
 
     @Override
