dcf0f18e8cc51fe94940381aab133ee580a4ebdb
bala-amypo <bala@amypo.in>
2025-12-18 14:35:59 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,3 +1,25 @@
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .name("Authorization")
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+        return new OpenAPI()
+            .info(new Info()
+                .title("SaaS User Role Permission Manager API")
+                .version("1.0")
+                .description("JWT secured role-permission management"))
+            .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+            .schemaRequirement("Authorization", securityScheme);
+    }
+}
+
+
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
