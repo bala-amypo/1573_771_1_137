dd74c7ab13fcbe52d9471444fa6f78baaf72ba6b
bala-amypo <bala@amypo.in>
2025-12-18 04:30:02 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountservice.java
+++ src/main/java/com/example/demo/service/UserAccountservice.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.UserRole;
+import com.example.demo.entity.UserAccount;
 import java.util.List;
 
-public interface UserRoleService {
-    UserRole assignRole(UserRole mapping);
-    List<UserRole> getRolesForUser(Long userId);
-    UserRole getMappingById(Long id);
-    void removeRole(Long mappingId);
+public interface UserAccountService {
+    UserAccount createUser(UserAccount user);
+    UserAccount updateUser(Long id, UserAccount user);
+    UserAccount getUserById(Long id);
+    List<UserAccount> getAllUsers();
+    void deactivateUser(Long id);
 }
